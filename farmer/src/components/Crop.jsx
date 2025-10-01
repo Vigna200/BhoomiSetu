a1539c7b7eaa0ad6365c665ef66652e5ae967fb9
Vigna200 <161219850+Vigna200@users.noreply.github.com>
2025-10-02 01:46:15 +0530

    Crop.jsx
    Linked with ai model

--- farmer/src/components/Crop.jsx
+++ farmer/src/components/Crop.jsx
@@ -59,7 +59,7 @@ function Crop() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict/crop", {
+      const res = await fetch("https://bhoomisetu-rvtj.onrender.com/predict/crop", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
